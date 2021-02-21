4eef889b0a082109fa31e5b8f63f11830638f005
Barbora Doslikova <doslikovab@gmail.com>
2021-02-21 12:00:29 +0100

    catch ValueError exception

--- odd-or-even.py
+++ odd-or-even.py
@@ -1,31 +1,30 @@
 # -*- coding: utf-8 -*-
 """
-Ask the user for a number.
+Asks the user for a number.
 Depending on whether the number is even or odd,
-print out an appropriate message to the user. 
+prints out an appropriate message to the user. 
 
 Even number is an integer (i.e. not a fraction or decimal)
 divisible by 2 without a remainder,
 including a 0.
 
+Catches ValueError exception,
+e.g. when an integer was not entered.32
+
 By Barbora Doslikova.
 """
-
-userNumber = int(input("Give any whole number: "))
-
-"""
-while userNumber <= 0:
-    userNumber = float(input("Please, give any positive whole number bigger than 0: "))
+try:  
+    userNumber = int(input("Give any whole number: ")) 
+except ValueError:
+    print("please re-run the script and try to enter a valid whole number")
+except:
+    print("something else went wrong, please try again")
 else:
-    userNumber = userNumber
- """   
+    workingNumber = userNumber/2 - userNumber//2
+        
+    if workingNumber == 0:
+        output = "is an EVEN number"
+    else:
+        output = "is is an ODD number"     
     
-workingNumber = userNumber/2 - userNumber//2
-    
-if workingNumber == 0:
-    output = "is an EVEN number"
-else:
-    output = "is is an ODD number"
-
-
-print(userNumber, output)
+    print(userNumber, output)
