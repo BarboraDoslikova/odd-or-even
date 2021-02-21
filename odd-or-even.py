94db6a64815866019b4c8c68744c564deeedc41f
Barbora Doslikova <doslikovab@gmail.com>
2021-02-21 11:46:13 +0100

    works for integers

--- odd-or-even.py
+++ odd-or-even.py
@@ -4,26 +4,28 @@ Ask the user for a number.
 Depending on whether the number is even or odd,
 print out an appropriate message to the user. 
 
-This is a temporary script file.
+Even number is an integer (i.e. not a fraction or decimal)
+divisible by 2 without a remainder,
+including a 0.
+
+By Barbora Doslikova.
 """
-import math
 
+userNumber = int(input("Give any whole number: "))
 
-userNumber = float(input("Give any positive number: "))
-
-
+"""
 while userNumber <= 0:
-    userNumber = float(input("Give any *positive* number: "))
+    userNumber = float(input("Please, give any positive whole number bigger than 0: "))
 else:
     userNumber = userNumber
+ """   
     
+workingNumber = userNumber/2 - userNumber//2
     
-workingNumber = userNumber / 2 - math.floor(userNumber / 2)
-    
-if workingNumber == 0.0:
-    output = " is an EVEN number"
+if workingNumber == 0:
+    output = "is an EVEN number"
 else:
-    output = " is is not an even number"
+    output = "is is an ODD number"
 
 
 print(userNumber, output)
